e293eecb5428337a864c25332e7d87f9156b6873
Jefferson Onofre Matavello <jeff_jom@hotmail.com>
2023-03-31 23:12:50 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/jefferson/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.jefferson.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jefferson.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
